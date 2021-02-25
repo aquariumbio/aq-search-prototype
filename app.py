06fd08b10acb963fb2e8047564c434cba00b09e7
Devin Strickland <strcklnd@uw.edu>
2021-02-25 14:19:44 -0500

    added basic html

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import time
 
 import redis
-from flask import Flask
+from flask import Flask, render_template
 
 from util.search_prototype import search
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-@app.route('/')
+@app.route('/', methods=("GET", "POST"))
 def sample_search():
     terms = "norn unfolded singer"
     results = search(terms, offset=0, limit=20, method='or')
-    return results
+    return render_template('search.html', results=results)
