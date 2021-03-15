b022b638959fb49e52f57e4b8203c19ecfad24a1
Devin Strickland <strcklnd@uw.edu>
2021-03-15 09:26:08 -0400

    removed unused redis

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import time
 
-import redis
 from flask import Flask, render_template, request
 
 from util.search_prototype import search, list_samples
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
 @app.route('/', methods=("GET", "POST"))
 def sample_search():
