ddaeb1fd01507c31d9265140ca64854232f506e1
Devin Strickland <strcklnd@uw.edu>
2021-03-18 13:42:40 -0400

    return job operations

--- util/data_queries.py
+++ util/data_queries.py
@@ -3,25 +3,33 @@ from util.connection import get_connection, fetch_sql
 def data_for_item(item_id = None):
     connection = get_connection()
     with connection:
-        job_ids = jobs_producing(item_id)
-        # job_items = fetch_job_items(connection)
-        return job_ids
+        jobs = jobs_producing(item_id)
+        for job in jobs:
+            operations = fetch_job_operations(job["id"])
+            job["operation_ids"] = [op["id"] for op in operations]
+        return jobs
 
 def jobs_producing(item_id):
-    sql = "SELECT jbs.id FROM " \
-              "(SELECT parent_id FROM field_values " \
+    sql = "SELECT jbs.id, jbs.state, fvs.name AS output_name FROM " \
+              "(SELECT parent_id, name FROM field_values " \
               "WHERE parent_class = 'Operation' " \
-              "AND role = 'input' " \
-              "AND child_item_id = '{}') AS fvs " \
+              "AND role = 'output' " \
+              "AND child_item_id = '{item_id}') AS fvs " \
           "INNER JOIN operations AS ops " \
           "ON ops.id = fvs.parent_id " \
           "INNER JOIN job_associations AS jas " \
           "ON jas.operation_id = ops.id " \
           "INNER JOIN jobs AS jbs " \
           "ON jbs.id = jas.job_id " \
-          "ORDER BY jbs.id".format(item_id)
+          "ORDER BY jbs.id".format(item_id = item_id)
 
     return fetch_sql(sql)
 
-def fetch_job_items(connection):
-    pass
+def fetch_job_operations(job_id):
+    sql = "SELECT operations.id " \
+          "FROM operations " \
+          "JOIN job_associations " \
+          "ON job_associations.operation_id = operations.id " \
+          "WHERE job_associations.job_id = {job_id}".format(job_id = job_id)
+
+    return fetch_sql(sql)
