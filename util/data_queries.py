071af7476e06a814be1a3ffb63ffd6b04ff55097
Devin Strickland <strcklnd@uw.edu>
2021-04-06 13:23:34 -0400

    add some more queries, make sure connection is passed properly

--- util/data_queries.py
+++ util/data_queries.py
@@ -1,15 +1,19 @@
+import json
 from util.connection import get_connection, fetch_sql
 
 def data_for_item(item_id = None):
     connection = get_connection()
     with connection:
-        jobs = jobs_producing(item_id)
+        jobs = jobs_producing(item_id, connection)
         for job in jobs:
-            operations = fetch_job_operations(job["id"])
-            job["operation_ids"] = [op["id"] for op in operations]
-        return jobs
+            operations = fetch_job_operations(job["id"], connection)
+            job["operations"] = operations
+            state = json.loads(job.pop("state"))
+            job["state"] = state[-1]["operation"]
 
-def jobs_producing(item_id):
+    return jobs
+
+def jobs_producing(item_id, connection):
     sql = "SELECT jbs.id, jbs.state, fvs.name AS output_name FROM " \
               "(SELECT parent_id, name FROM field_values " \
               "WHERE parent_class = 'Operation' " \
@@ -23,13 +27,34 @@ def jobs_producing(item_id):
           "ON jbs.id = jas.job_id " \
           "ORDER BY jbs.id".format(item_id = item_id)
 
-    return fetch_sql(sql)
+    return fetch_sql(sql, connection)
 
-def fetch_job_operations(job_id):
-    sql = "SELECT operations.id " \
+def fetch_job_operations(job_id, connection):
+    sql = "SELECT operations.id, operations.operation_type_id " \
           "FROM operations " \
           "JOIN job_associations " \
           "ON job_associations.operation_id = operations.id " \
           "WHERE job_associations.job_id = {job_id}".format(job_id = job_id)
+
+    return fetch_sql(sql, connection)
+
+def find_by_id(model, id):
+    sql = "SELECT {model}s.* FROM {model}s " \
+          "WHERE {model}s.id = {id} LIMIT 1".format(model = model, id = id)
+
+    results = fetch_sql(sql)
+    if results: return results[0]
+
+def io_for(operation_id):
+    sql = "SELECT field_values.* FROM field_values " \
+          "WHERE parent_class = 'Operation' " \
+          "AND parent_id = '{operation_id}'".format(operation_id = operation_id)
+
+    return fetch_sql(sql)
+
+def data_associations_for(parent_class, parent_id):
+    sql = "SELECT * FROM data_associations " \
+          "WHERE parent_class = '{parent_class}' " \
+          "AND parent_id = '{parent_id}'".format(parent_class = parent_class, parent_id = parent_id)
 
     return fetch_sql(sql)
