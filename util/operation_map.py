eddf173c5412ce339e9ea7dae8ba30e47d35a208
Devin Strickland <strcklnd@uw.edu>
2021-04-12 10:48:26 -0400

    added some documentation

--- util/operation_map.py
+++ util/operation_map.py
@@ -18,10 +18,10 @@ class OperationMap():
         * Data attached to input or output Items
         * Data attached directly to the Operation
     Data structures returned by this class do not recognize differences among these
-    types, or differences in capitalization for keys that retrieve them. For example,
-    the name of a sample from an input labeled 'Polymerase' would be added the same
-    list as the value of a DataAssociation with the key 'polymerase'. This list would
-    be retrieved using the key 'polymerase'.
+    types, or differences in capitalization for keys that retrieve them. All keys
+    are converted to snake_case. For example, the name of a sample from an input labeled
+    'Polymerase' would be added the same list as the value of a DataAssociation with
+    the key 'polymerase'. This list would be retrieved using the key 'polymerase'.
 
     @author Devin Strickland <strcklnd@uw.edu>
     """
@@ -37,16 +37,25 @@ class OperationMap():
         self.__operation_data: Dict[str, list] = {}
 
     def name(self) -> str:
+        """The name of the OperationType"""
         return self.operation_type()["name"]
 
     def id(self) -> int:
+        """The ID of the Operation"""
         return self.__operation["id"]
 
     def operation_type(self) -> dict:
+        """The OperationType"""
         return find_by_id("operation_type",
                           self.__operation["operation_type_id"])
 
-    def all_keys(self) -> list:
+    def all_keys(self) -> List[str]:
+        """
+        Gets all keys for all types of data associated with the Operation.
+
+        Keys include DataAssociation keys and Operation I/O names. All keys are converted
+        to snake_case.
+        """
         return sorted(list(set(
             list(self.input_samples().keys()) +
             list(self.input_parameters().keys()) +
@@ -57,6 +66,11 @@ class OperationMap():
         )))
 
     def fetch_data(self, key: str) -> list:
+        """
+        Gets all data for a given key that is associated with the Operation.
+
+        The provided key should be in snake_case.
+        """
         data = OperationMap.flatten([
             self.input_samples().get(key),
             self.input_parameters().get(key),
@@ -84,12 +98,12 @@ class OperationMap():
     def item_outputs(self) -> List[FieldValue]:
         return [o for o in self.outputs() if o["child_item_id"]]
 
-    def input_samples(self) -> Dict[str, list]:
+    def input_samples(self) -> Dict[str, List[str]]:
         if not self.__input_samples:
             self.__input_samples = self.collect_samples(self.inputs())
         return self.__input_samples
 
-    def output_samples(self) -> Dict[str, list]:
+    def output_samples(self) -> Dict[str, List[str]]:
         if not self.__output_samples:
             self.__output_samples = self.collect_samples(self.outputs())
         return self.__output_samples
@@ -116,8 +130,8 @@ class OperationMap():
             self.__operation_data = data
         return self.__operation_data
 
-    def collect_samples(self, field_values: List[FieldValue]) -> Dict[str, list]:
-        samples: Dict[str, list] = {}
+    def collect_samples(self, field_values: List[FieldValue]) -> Dict[str, List[str]]:
+        samples: Dict[str, List[str]] = {}
         for fv in field_values:
             if not fv["child_sample_id"]: next
 
@@ -159,7 +173,8 @@ class OperationMap():
         if not obj.get(key): obj[key] = []
         obj[key].append(value)
 
-    def make_key(self, string: str) -> str:
+    @classmethod
+    def make_key(cls, string: str) -> str:
         key = str(string).strip().lower()
         return re.subn(OperationMap.key_replace(), '_', key)[0]
 
